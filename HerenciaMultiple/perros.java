fe307fcc2a650cfba8780d2980503bb69940452f
Marcouzis11 <127408024+Marcouzis11@users.noreply.github.com>
2023-11-24 11:51:58 -0300

    Prueba

---
+++ HerenciaMultiple/perros.java
@@ -0,0 +1,13 @@
+package HerenciaMultiple;
+/*
+ * #Perros va a ser la clase que se va a heredar.
+ * #Va a tener métodos normales, nada del otro mundo.
+*/
+public class perros {
+    //Como no le pongo constructor, el compilador se lo crea solo.
+
+    public void ladrar()
+    {
+        System.out.println("Buaf! (ladrido)");
+    }
+}
